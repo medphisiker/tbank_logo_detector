18e31cdbc27890c64fe5244dd46b6992a52a8140
Anton Shiryaev <med.phisiker@gmail.com>
2025-09-18 00:36:24 +1000

    обновление

--- data_preparation/synthesis/docker-build-run.bat
+++ data_preparation/synthesis/docker-build-run.bat
@@ -1,8 +1,22 @@
 @echo off
 REM Docker pull and run for synthesis test (small N=10) with pre-built image on Windows
 
+REM Переходим в корень проекта
+cd /d "%~dp0..\.."
+
 REM Альтернатива: Соберите свой образ
-REM docker build -t tbank-synth -f Dockerfile .
+REM docker build -t tbank-synth -f data_preparation/synthesis/Dockerfile data_preparation/synthesis
 
 docker pull medphisiker/tbank-synth:latest
-docker run -v "%cd%:/app/synthesis" -v "%cd%/../../data:/app/data" --rm medphisiker/tbank-synth python gen_synth.py --N 10
+
+REM Нормализуем пути для Docker (заменяем \ на /)
+set "ROOT_PATH=%cd:/=/%"
+set "SYNTH_PATH=%ROOT_PATH%/data_preparation/synthesis"
+set "DATA_PATH=%ROOT_PATH%/data"
+
+REM Отладка путей
+echo Root: %ROOT_PATH%
+echo Synthesis: %SYNTH_PATH%
+echo Data: %DATA_PATH%
+
+docker run -v "%SYNTH_PATH%:/app/synthesis" -v "%DATA_PATH%:/app/data" --rm medphisiker/tbank-synth python gen_synth.py --N 10
